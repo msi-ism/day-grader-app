4868faa45dd068684b068529cdb62e4563a94c41
msi-ism <msism720@gmail.com>
2022-12-27 17:27:19 -0500

    working on how to render tile content

--- src/components/CalendarComponent.js
+++ src/components/CalendarComponent.js
@@ -3,11 +3,18 @@ import Calendar from 'react-calendar';
 import '../css/CalendarCss.css'
 import { useState } from 'react';
 
-const CalendarComponent = () => {
+
+const CalendarComponent = (props) => {
+    console.log(props)
+    const selectedDate = props.date
+    console.log(selectedDate)
     const [value, onChange] = useState(new Date())
     return (
         <div>
             <Calendar onChange={onChange} value={value} />
+            <div className='current-date'>
+                <h1>The Selected Date is:{selectedDate}</h1>
+            </div>
         </div>
     );
 }
