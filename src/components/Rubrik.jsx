f5ca26fa9e611bf436eac75aef00c11fd71a55d8
msi-ism <msism720@gmail.com>
2023-01-19 18:11:58 -0500

    ui updates

--- src/components/Rubrik.jsx
+++ src/components/Rubrik.jsx
@@ -14,9 +14,11 @@ const Rubrik = () => {
                     <li>Task 3</li>
                     <li>Task 4</li>
                 </ul>
+                <button>Grade your Day</button>
             </div>
             <div className='gpa-container'>
                 <h2>Current GPA:</h2>
+                <h4>3.75</h4>
             </div>
             
         </div>
