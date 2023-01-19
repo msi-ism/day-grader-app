c3fcd0a01cd68a48062e2fad06b20710bb4f81b3
msi-ism <msism720@gmail.com>
2023-01-19 18:05:08 -0500

    front end coming together

--- src/components/Rubrik.jsx
+++ src/components/Rubrik.jsx
@@ -7,10 +7,16 @@ const Rubrik = () => {
                 <h2>Rubrik</h2>
             </div>
             <div className='criteria-container'>
-                <h2>Hello Criteria</h2>
+                <h2>Daily Tasks</h2>
+                <ul>
+                    <li>Testing out a long Task 1</li>
+                    <li>Task 2</li>
+                    <li>Task 3</li>
+                    <li>Task 4</li>
+                </ul>
             </div>
             <div className='gpa-container'>
-                <h2>Hello GPA</h2>
+                <h2>Current GPA:</h2>
             </div>
             
         </div>
