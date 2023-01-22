caa1c5980254fc843eecfe993fabbadbf78d9837
msi-ism <msism720@gmail.com>
2023-01-22 10:36:15 -0500

    temporarily removed sign-up & user auth

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,23 @@
 import { Link } from 'react-router-dom'
 import '../css/NavBar.css'
 import logo from '../images/grade.png'
+import * as userService from '../utilities/users-service'
 
 
 
 const NavBar = ({user, setUser}) => {
     {console.log(user)}
-    // const handleLogOut = () => {
-    //     userService.logOut()
-    //     setUser(null)
-    // }
+    const handleLogOut = () => {
+        userService.logOut()
+        setUser(null)
+    }
     return(
         <nav className='navbar'>
             <div className='navlogo'>
                 <img className='logo' src={logo} alt='Logo'/>
             </div>
             <div className='navmenu'>
-                {user ? <><span className='navname'>  Welcome, {user.name} Log Out</span></> : ''}
+            {user ? <><span className='navname'>  Welcome, {user.name} <button className='navname' onClick={handleLogOut}>Log Out</button></span></> : ''}
             </div>
         </nav>
     )
