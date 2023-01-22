931cdd8425492ec882cb7c8eb704734e53a6b594
msi-ism <msism720@gmail.com>
2023-01-22 12:35:44 -0500

    added task submission box

--- src/components/Rubrik.jsx
+++ src/components/Rubrik.jsx
@@ -2,27 +2,36 @@ import React from 'react';
 import {useState, useEffect} from 'react'
 
 const Rubrik = () => {
-    const [tasks, setTasks] = useState([])
-    tasks.push('Practice guitar', 'drink 32oz of water', 'stunt on these heaux', 'try to take over the world')
+    const [tasks, setTasks] = useState({
+        tasks: '',
+
+    })
+  
+    const [error, setError] = useState('')
+
+    const handleChange = (evt) => {
+        setTasks({...tasks, [evt.target.name]: evt.target.value})
+    }
+
     console.log(tasks)
-
-
     return (
         <div className='rubrik-container'>
             <div className='rubrik-header'>
                 <h2>Rubrik</h2>
             </div>
             <div className='criteria-container'>
-                <h2>Daily Tasks</h2>
+                <h2>Daily Goals</h2>
                 <ul className='task-list'>
-                    {tasks.map((task, idx) => (
+                    {/* {tasks.map((task, idx) => (
                         <li key={idx} className='task-item'>
                             <p>{task}</p>
                         </li>
-                    ))}
-    
+                    ))} */}
                 </ul>
-                <button>Grade your Day</button>
+                <div className='task-submit'>
+                <input type='text' placeholder='Add a task' onChange={handleChange}></input>
+                <button>Add</button>
+                </div>
             </div>
             <div className='gpa-container'>
                 <h2>Current GPA:</h2>
